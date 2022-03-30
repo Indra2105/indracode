e035bf17aec9612ebbd352a7596b68e9c00bbc88
Stevan Camargo <juanstevancamargoochoa@gmail.com>
2022-03-29 23:37:46 -0500

    Primer Commit

---
+++ parte1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Mensaje from './mensaje.js'
+
+const Descripcion = () => {
+  return (<h1>estos son los colores de mi hermosa colombia</h1>)
+}
+
+const App= () => {
+  return(
+    <div className="App">
+      
+    </div>
+  );
+
+}
+
+export default App;
